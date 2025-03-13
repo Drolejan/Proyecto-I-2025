156aa80f7adfb413dcadcc74e4353b85c333e278
Alejandro Medina <alejandro.medina@ucslp.net>
2025-03-13 13:30:05 -0600

    cambio score

--- Assets/saque.cs
+++ Assets/saque.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using TMPro;//Con esto importamos comandos de TextMeshPro
 
 public class saque : MonoBehaviour
 {
+    public int scoreP1,scoreP2;//Variables numericas
+    public TextMeshProUGUI textoP1,textoP2;//Variables de texto
     Rigidbody2D pelotaRB;//Este es el rigidbody de la pelota
     public float fuerza;
     public bool playing;
@@ -21,8 +24,20 @@ public class saque : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        playing=false;
-        pelotaRB.linearVelocity=Vector2.zero;
-        transform.position=inicio.position;
+        if(collision.gameObject.CompareTag("golP1")){
+            playing=false;
+            pelotaRB.linearVelocity=Vector2.zero;
+            transform.position=inicio.position;
+            scoreP1++;//Le sumamos 1 al score
+            textoP1.text="Jugador1: "+scoreP1.ToString();//Cambiamos el texto
+        }
+        if(collision.gameObject.CompareTag("golP2")){
+            playing=false;
+            pelotaRB.linearVelocity=Vector2.zero;
+            transform.position=inicio.position;
+            scoreP2++;//Le sumamos 1 al score
+            textoP2.text="Jugador2: "+scoreP2.ToString();//Cambiamos el texto
+        }
+        
     }
 }
